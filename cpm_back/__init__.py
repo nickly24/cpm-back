416e9ae744d07050c65dbd752e7c9665db26399c
Николай Прибыш <nikolajpribys@MacBook-Air-Nikolaj.local>
2026-07-18 23:09:46 +0300

    fix: consume homework runner lock result

--- cpm_back/__init__.py
+++ cpm_back/__init__.py
@@ -43,7 +43,13 @@ from .blueprints import (
 from cpm_back.blueprints.test_attempts_bp import test_attempts_bp
 
 _LOG_MOSCOW_TZ = ZoneInfo('Europe/Moscow')
-logging.Formatter.converter = lambda timestamp: datetime.fromtimestamp(timestamp, _LOG_MOSCOW_TZ).timetuple()
+
+
+def _moscow_log_converter(timestamp):
+    return datetime.fromtimestamp(timestamp, _LOG_MOSCOW_TZ).timetuple()
+
+
+logging.Formatter.converter = staticmethod(_moscow_log_converter)
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(message)s',
